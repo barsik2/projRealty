9d6696eec4ef85bcbcf9b64bda1aa5bdcf939213
Arslan <arslan.bovaeff@yandex.ru>
2023-06-15 21:29:13 +0300

    update adress

--- frontend/src/pages/CategoryPages.js
+++ frontend/src/pages/CategoryPages.js
@@ -5,91 +5,16 @@ import "./CategoryPages.css";
 import Footer from "../components/Footer/Footer";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { CARD_ROUTE } from "../constRoute/consts";
-
-function DropdownButton({ handleFloorsFilter, handleGaragesFilter }) {
-  // Код для DropdownButton
-  const [menuOpenFloors, setMenuOpenFloors] = useState(false);
-  const oneFloors = 1;
-  const twoFloors = 2;
-  const threeFloors = 3;
-
-  const handleMenuToggleFloors = () => {
-    setMenuOpenFloors(!menuOpenFloors);
-  };
-
-  const handleFloorsSelect = (floors) => {
-    setMenuOpenFloors(false);
-    handleFloorsFilter(floors);
-  };
-
-
-  return (
-    <div className="dropdown">
-      <button className="dropdown-button" onClick={handleMenuToggleFloors}>
-        <p className="text_dropdown">Этажи</p>
-        <svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 0.961529L9.04163 8.27674C8.64746 8.69112 7.98668 8.69112 7.59252 8.27674L1 1.34614" stroke="black" strokeLinecap="round"/>
-        </svg>
-      </button>
-      {menuOpenFloors && (
-        <div className="dropdown-menu">
-          <button onClick={() => handleFloorsSelect(oneFloors)}>{oneFloors}</button>
-          <button onClick={() => handleFloorsSelect(twoFloors)}>{twoFloors}</button>
-          <button onClick={() => handleFloorsSelect(threeFloors)}>{threeFloors}</button>
-        </div>
-      )}
-
-    </div>
-  );
-}
-
-function DropdownButtonGarages({ handleGaragesFilter }) {
-  // Код для DropdownButtonGarages
-  const [menuOpenGarages, setMenuOpenGarages] = useState(false);
-  const noGarages = 0;
-  const oneGarage = 1;
-  const twoGarages = 2;
-
-  const handleMenuToggleGarages = () => {
-    setMenuOpenGarages(!menuOpenGarages);
-  };
-
-  const handleGaragesSelect = (garages) => {
-    setMenuOpenGarages(false);
-    handleGaragesFilter(garages);
-  };
-
-
-  return (
-    <div className="dropdown">
-      <button className="dropdown-button" onClick={handleMenuToggleGarages}>
-        <p className="text_dropdown">Гаражи</p>
-        <svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 0.961529L9.04163 8.27674C8.64746 8.69112 7.98668 8.69112 7.59252 8.27674L1 1.34614" stroke="black" strokeLinecap="round"/>
-        </svg>
-      </button>
-      {menuOpenGarages && (
-        <div className="dropdown-menu">
-          <button onClick={() => handleGaragesSelect(noGarages)}>Нет</button>
-          <button onClick={() => handleGaragesSelect(oneGarage)}>1</button>
-          <button onClick={() => handleGaragesSelect(twoGarages)}>2</button>
-        </div>
-      )}
-
-    </div>
-  );
-}
+import { CARD_ROUTE, CATEGORY_ROUTE, HOME_ROUTE } from "../constRoute/consts";
 
 const fetchData = async () => {
   try {
-    // Код для fetchData
     axios.defaults.headers.common['ngrok-skip-browser-warning'] = true;
     axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
     axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
     axios.defaults.headers.common['Access-Control-Allow-Methods'] = '*';
     axios.defaults.headers.common['Content-Type'] = 'text/html; charset=utf-8';
-    const response = await axios.get('https://terned.loca.lt/api/house');
+    const response = await axios.get('http://195.24.67.222:5000/api/house');
     return response.data.rows;
   } catch (error) {
     console.error(error);
@@ -103,7 +28,7 @@ const CategoryPages = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://terned.loca.lt/api/house');
+        const response = await axios.get('http://195.24.67.222:5000/api/house');
         setData(response.data.rows);
       } catch (error) {
         console.error(error);
@@ -112,48 +37,50 @@ const CategoryPages = () => {
     fetchData();
   }, []);
 
-  const [floorsFilter, setFloorsFilter] = useState(0);
-  const [garagesFilter, setGaragesFilter] = useState(0);
-  const [projects, setProjects] = useState([]);
+  const [floors, setFloors] = useState('');
 
-  const handleFloorsFilter = (floors) => {
-    setFloorsFilter(floors);
-  };
+  const loadData = (floorsParam) => {
+    axios.defaults.headers.common['ngrok-skip-browser-warning'] = true;
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+    axios.defaults.headers.common['Access-Control-Allow-Methods'] = '*';
+    axios.defaults.headers.common['Content-Type'] = 'text/html; charset=utf-8';
+    axios.get(`http://195.24.67.222:5000/api/house/filters?floors=${floorsParam}`)
+      .then(response => setData(response.data.rows))
+      .catch(error => console.log(error));
+  }
 
-  const handleGaragesFilter = (garages) => {
-    setGaragesFilter(garages);
-  };
+  const handleDropdownChange = (event) => {
+    setFloors(event.target.value)
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        `https://terned.loca.lt/api/house/filters?floors=${floorsFilter}&garages=${garagesFilter}`
-      );
-      setProjects(response.data);
-    };
-
-    if (floorsFilter !== null || garagesFilter !== null) {
-      fetchData();
+    if (floors === '') {
+      setData([]);
+    } else {
+      loadData(floors);
     }
-  }, [floorsFilter, garagesFilter]);
+  }, [floors]);
 
   return (
     <div className="main_container_category">
       <Header />
       <h1 className="txt_catalog">КАТАЛОГ ПРОЕКТОВ</h1>
-      <div className="dropdown_buttons_container">
-        <DropdownButton handleFloorsFilter={handleFloorsFilter} />
-        <DropdownButtonGarages handleGaragesFilter={handleGaragesFilter} />
+      <div className='filters'>
+        <select value={floors} onChange={handleDropdownChange}>
+          <option value="">этажи</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
       </div>
-
       <div className="m_c_container">
-        {data.map((item) => (
+        {data && data.map((item) => (
           <div className="container_card_category" key={item.id}>
             {item.img_title !== null && (
               <Link className="first_card_category" to={CARD_ROUTE}>
                 <img
                   className="img_img_title"
-                  src={`https://terned.loca.lt/${item.img_title}`}
+                  src={`http://195.24.67.222:5000/${item.img_title}`}
                   alt="a"
                 />
               </Link>
